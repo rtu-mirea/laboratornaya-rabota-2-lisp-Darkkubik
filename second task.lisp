c1a65943d59444de7da680b29b4b17b9eee01ada
Darkkubik <45105081+Darkkubik@users.noreply.github.com>
2019-12-26 10:56:39 +0300

    Update and rename second task to second task.lisp

--- second task.lisp
+++ second task.lisp
@@ -1,7 +1,9 @@
+;;вывести на печать все строки файла
 (defun out (path) 
 (let ((in (open path :if-does-not-exist nil))) 
 (when in 
 (loop for line = (read-line in nil) 
 while line do (format t "~a~%" line)) 
-(close in)))) 
+(close in))))
+
 (out ("test.txt")) 
